c74c865a8ed6f2e58fd0d435002b42e76ca08024
Ryan Richard <ryanrichard07@gmail.com>
2013-08-09 16:31:41 -0500

    move rsyslog changes to it's own recipe

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'ryan.richard@rackspace.com'
 license          'All rights reserved'
 description      'Installs/Configures logstash-rolebook'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.3.9'
+version          '0.4.0'
 
 #chef-logstash handles most dependencies
 depends 'apt'
--- recipes/logstash_agent.rb
+++ recipes/logstash_agent.rb
@@ -8,30 +8,3 @@ node.default['logstash']['agent']['xmx'] = '300M'
 
 include_recipe "apt"
 include_recipe "chef-logstash::agent"
-
-# check if running chef-solo.  If not, detect the logstash server/ip by role.  If I can't do that, fall back to using ['logstash']['agent']['server_ipaddress']
-if Chef::Config[:solo]
-  logstash_server_ip = node['logstash']['agent']['server_ipaddress']
-else
-  logstash_server_results = search(:node, "roles:#{node['logstash']['agent']['server_role']}")
-  unless logstash_server_results.empty?
-    logstash_server_ip = logstash_server_results[0]['ipaddress']
-  else
-    logstash_server_ip = node['logstash']['agent']['server_ipaddress']
-  end
-end
-
-template "/etc/rsyslog.d/99-logstash.conf" do
-	source "99-logstash.conf.erb"
-	owner "root"
-	group "root"
-	mode 00644
-	variables ({
-		:logstash_server_ip => logstash_server_ip
-	})
-end
-
-service "rsyslog" do
-	supports :status => true, :restart => true
-	action [ :restart ]
-end
