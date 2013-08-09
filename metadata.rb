301af5b7682b3d78791dc20dffce0efff3937459
Ryan Richard <ryanrichard07@gmail.com>
2013-08-09 11:21:26 -0500

    lower ram given to logstash agent

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'ryan.richard@rackspace.com'
 license          'All rights reserved'
 description      'Installs/Configures logstash-rolebook'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.3.7'
+version          '0.3.8'
 
 #chef-logstash handles most dependencies
 depends 'apt'
--- recipes/logstash_agent.rb
+++ recipes/logstash_agent.rb
@@ -2,6 +2,8 @@ node.default['logstash']['agent']['version'] = '1.1.13'
 node.default['logstash']['agent']['base_config_cookbook'] = 'logstash-rolebook'
 node.default['logstash']['agent']['base_config'] = 'rax.agent.conf.erb'
 node.default['logstash']['agent']['server_ipaddress'] = '127.0.0.1'
+node.default['logstash']['agent']['xms'] = '1M'
+node.default['logstash']['agent']['xmx'] = '100M'
 
 
 include_recipe "apt"
